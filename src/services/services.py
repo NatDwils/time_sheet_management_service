c7a826361d88a8aa02567ad21a78fec6462257ee
Ishika7890 <ishikanimade56@gmail.com>
2021-11-18 18:33:22 +0530

    final

--- src/services/services.py
+++ src/services/services.py
@@ -1,19 +1,15 @@
 from fastapi import APIRouter, File, UploadFile
-
 from src.models.user import User
 from src.repository.db import task
 import pandas as pd
-from bson.objectid import ObjectId
-from src.schemas.user import userEntity, usersEntity
+from src.resource.task import get_task,get_task_sheet,update_task
 
 user = APIRouter()
 
 
 @user.get('/getall_task_sheet/')
 async def get_task_sheet():
-    print(task.find())
-    print(usersEntity(task.find()))
-    return usersEntity(task.find())
+   get_task_sheet()
 
 
 @user.post("/upload_task_sheet/")
@@ -29,17 +25,9 @@ async def create_task_sheet(excel_file: UploadFile = File(...)):
 
 @user.put('/{id}')
 async def update_task(id, task_update_request: User):
-    task.update_one({"_id": ObjectId(id)},
-                    {"$set": {"name": task_update_request.name, "task": task_update_request.task,
-                              "start_date": task_update_request.start_date, "end_date": task_update_request.end_date}})
-    return userEntity(task.find_one({"_id": ObjectId(id)}))
-
-
-@user.get('/{id}')
-async def get_task(id):
-    return userEntity(task.find_one({"_id": ObjectId(id)}))
+   update_task()
 
 
 @user.get('/{user_id}')
-async def get_task(id, start_date: int = 0, end_date: int = 10):
-    return userEntity(task.find_one({"_id": ObjectId(id)}))
+async def get_task(id, start_date: int = 0, end_date: int = 10):\
+    get_task()
